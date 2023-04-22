5881a229c21b5a893566fa4dfb4016049a02792e
santhoshagganur <agganursanthosh@gmail.com>
2023-04-22 18:10:57 +0000

    on progress

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Login from './components/Login'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
+import JobItemDetails from './components/JobItemDetails'
 import NotFound from './components/NotFound'
 
 // These are the lists used in the application. You can move them to any component needed.
@@ -13,6 +14,7 @@ const App = () => (
     <Route exact path="/login" component={Login} />
     <Route exact path="/" component={Home} />
     <Route exact path="/jobs" component={Jobs} />
+    <Route exact path="/jobs/:id" component={JobItemDetails} />
     <Route exact path="/bad-path" component={NotFound} />
     <Route component={NotFound} />
   </Switch>
--- src/components/JobItemDetails/index.js
+++ src/components/JobItemDetails/index.js
@@ -1,6 +1,9 @@
 import {Component} from 'react'
 import './index.css'
 import Loader from 'react-loader-spinner'
+import {AiFillStar} from 'react-icons/ai'
+import {GoLocation} from 'react-icons/go'
+import {BsBriefcaseFill} from 'react-icons/bs'
 import Cookies from 'js-cookie'
 import Header from '../Header'
 
@@ -12,11 +15,34 @@ const apiStatusConstants = {
 }
 
 class JobItemDetails extends Component {
-  state = {apiStatus: apiStatusConstants.initial}
+  state = {
+    apiStatus: apiStatusConstants.initial,
+    companyData: {},
+    similarJobs: [],
+  }
 
   componentDidMount() {
     this.getJobItem()
   }
+
+  getCompanyData = data => ({
+    companyLogoUrl: data.company_logo_url,
+    companyWebsiteUrl: data.company_website_url,
+    employmentType: data.employment_type,
+    id: data.id,
+    jobDescription: data.job_description,
+    location: data.location,
+    packagePerAnnum: data.package_per_annum,
+    rating: data.rating,
+    lifeAtCompany: {
+      description: data.life_at_company.description,
+      imageUrl: data.life_at_company.image_url,
+    },
+    skills: data.skills.map(each => ({
+      imageUrl: each.image_url,
+      name: each.name,
+    })),
+  })
 
   getJobItem = async () => {
     this.setState({apiStatus: apiStatusConstants.inProgress})
@@ -34,8 +60,104 @@ class JobItemDetails extends Component {
     }
 
     const response = await fetch(url, options)
-    console.log(response)
+    if (response.ok === true) {
+      const data = await response.json()
+      const updatedCompanyData = this.getCompanyData(data.job_details)
+      const similarJobsData = data.similar_jobs.map(each => ({
+        companyLogoUrl: each.company_logo_url,
+        employmentType: each.employment_type,
+        id: each.id,
+        jobDescription: each.job_description,
+        location: each.location,
+        rating: each.rating,
+        title: each.title,
+      }))
+
+      this.setState({
+        apiStatus: apiStatusConstants.success,
+        companyData: updatedCompanyData,
+        similarJobs: similarJobsData,
+      })
+    } else {
+      this.setState({apiStatus: apiStatusConstants.failure})
+    }
   }
+
+  renderSuccessView = () => {
+    const {companyData, similarJobs} = this.state
+    const {
+      companyLogoUrl,
+      location,
+      rating,
+      jobDescription,
+      packagePerAnnum,
+      title,
+      employmentType,
+    } = companyData
+
+    return (
+      <div className="job-details-bottom-container">
+        <div className="job-item-details">
+          <div className="company-information">
+            <img
+              src={companyLogoUrl}
+              alt="job details company logo"
+              className="job-details-company-logo"
+            />
+            <div>
+              <h1 className="job-title"> {title} </h1>
+              <div className="company-information">
+                <AiFillStar className="star-icon" />
+                <p className="rating"> {rating} </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="job-location-details">
+            <div className="job-location">
+              <div className="location">
+                <GoLocation className="location-img" />
+                <p className="job-content"> {location} </p>
+              </div>
+              <div className="location">
+                <BsBriefcaseFill className="location-img" />
+                <p className="job-content"> {employmentType} </p>
+              </div>
+            </div>
+
+            <p className="salary"> {packagePerAnnum} </p>
+          </div>
+
+          <hr className="jobs-card-horizontal-line" />
+          <h1 className="description"> Description </h1>
+          <p className="job-description"> {jobDescription} </p>
+        </div>
+      </div>
+    )
+  }
+
+  renderFailureView = () => (
+    <div className="failure-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/failure-img.png"
+        alt="failure view"
+        className="failure-img"
+      />
+      <h1 className="failure-content"> Oops! Something Went Wrong </h1>
+      <p className="failure-reason">
+        We cannot seem to find the page you are looking for.
+      </p>
+      <button type="button" className="retry-btn" onClick={this.getJobs}>
+        Retry
+      </button>
+    </div>
+  )
+
+  renderLoader = () => (
+    <div className="loader-container" testid="loader">
+      <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
+    </div>
+  )
 
   renderJobDetails = () => {
     const {apiStatus} = this.state
