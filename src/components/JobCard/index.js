e87d50fee34a547a3184ebb1a2ccf1a927e8d079
santhoshagganur <agganursanthosh@gmail.com>
2023-04-27 13:30:29 +0000

    on progress

--- src/components/JobCard/index.js
+++ src/components/JobCard/index.js
@@ -2,6 +2,7 @@ import './index.css'
 import {AiFillStar} from 'react-icons/ai'
 import {GoLocation} from 'react-icons/go'
 import {BsBriefcaseFill} from 'react-icons/bs'
+import {Link} from 'react-router-dom'
 
 const JobCard = props => {
   const {jobDetails} = props
@@ -13,41 +14,48 @@ const JobCard = props => {
     employmentType,
     packagePerAnnum,
     jobDescription,
+    id,
   } = jobDetails
 
   return (
-    <li className="job-card-container">
-      <div className="job-title-container">
-        <img src={companyLogoUrl} alt="company logo" className="company-logo" />
-        <div>
-          <h1 className="job-title"> {title} </h1>
-          <div className="rating-container">
-            <AiFillStar className="rating-star" />
-            <p className="rating"> {rating} </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="job-location-details">
-        <div className="job-location">
-          <div className="location">
-            <GoLocation className="location-img" />
-            <p className="job-content"> {location} </p>
-          </div>
-          <div className="location">
-            <BsBriefcaseFill className="location-img" />
-            <p className="job-content"> {employmentType} </p>
+    <Link to={`/jobs/${id}`} className="link-item">
+      <li className="job-card-container">
+        <div className="job-title-container">
+          <img
+            src={companyLogoUrl}
+            alt="company logo"
+            className="company-logo"
+          />
+          <div>
+            <h1 className="job-title"> {title} </h1>
+            <div className="rating-container">
+              <AiFillStar className="rating-star" />
+              <p className="rating"> {rating} </p>
+            </div>
           </div>
         </div>
 
-        <p className="salary"> {packagePerAnnum} </p>
-      </div>
+        <div className="job-location-details">
+          <div className="job-location">
+            <div className="location">
+              <GoLocation className="location-img" />
+              <p className="job-content"> {location} </p>
+            </div>
+            <div className="location">
+              <BsBriefcaseFill className="location-img" />
+              <p className="job-content"> {employmentType} </p>
+            </div>
+          </div>
 
-      <hr className="jobs-card-horizontal-line" />
+          <p className="salary"> {packagePerAnnum} </p>
+        </div>
 
-      <h1 className="description"> Description </h1>
-      <p className="job-description"> {jobDescription} </p>
-    </li>
+        <hr className="jobs-card-horizontal-line" />
+
+        <h1 className="description"> Description </h1>
+        <p className="job-description"> {jobDescription} </p>
+      </li>
+    </Link>
   )
 }
 
